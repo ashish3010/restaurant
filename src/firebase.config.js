b972722871a0feb7268ce0b825594c52ac78d689
Ashish Mishra <69194675+ashish3010@users.noreply.github.com>
2022-08-01 15:56:14 +0530

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBDJ3SjmZB8ws9hgdJb0VbnBesp5pxZLZA",
-    authDomain: "restaurantapp-d5d28.firebaseapp.com",
-    databaseURL: "https://restaurantapp-d5d28-default-rtdb.firebaseio.com",
-    projectId: "restaurantapp-d5d28",
-    storageBucket: "restaurantapp-d5d28.appspot.com",
-    messagingSenderId: "743875317876",
-    appId: "1:743875317876:web:91da0506a6246aca024121"
+    apiKey: "your api key",
+    authDomain: "your auth domain",
+    databaseURL: "your firevase database url",
+    projectId: "your project id",
+    storageBucket: "your firebase storage bucket",
+    messagingSenderId: "firebase message sender id",
+    appId: "firebase app id"
   };
   
 
